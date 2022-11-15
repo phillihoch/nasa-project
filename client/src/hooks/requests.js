d4a49578a0368216d17ad9fcde982480cf06cee5
phillihoch <64873861+phillihoch@users.noreply.github.com>
2022-11-15 19:03:54 +0100

    add launches frontend implementation

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -10,15 +10,24 @@ async function httpGetPlanets() {
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
-  console.log(fetchedLaunches);
   return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber;
   });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return { ok: false };
+  }
 }
 
 async function httpAbortLaunch(id) {

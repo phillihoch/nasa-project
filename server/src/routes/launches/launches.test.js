e8a578e6fcaf09a3e1e343edf488f6d5f813aebd
phillihoch <64873861+phillihoch@users.noreply.github.com>
2022-11-16 17:10:09 +0100

    add jest implementation

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,13 @@
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+describe("Test POST /launches", () => {
+  test("It should respond with 201 created", () => {});
+
+  test("It should catch missing required properties", () => {});
+
+  test("It should catch invalid dates", () => {});
+});

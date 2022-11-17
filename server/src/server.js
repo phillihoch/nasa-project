e44021051f6bdfb5956966be6076a085f7cdb6d3
phillihoch <64873861+phillihoch@users.noreply.github.com>
2022-11-17 21:18:53 +0100

    add dotenv and remove mongo url

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,13 @@
 const http = require("http");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL =
-  "mongodb+srv://nasa-api:n1KWvsY37Zr7SQyh@nasacluster.3ivsroy.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
@@ -21,7 +20,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {

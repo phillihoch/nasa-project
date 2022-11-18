8e11e76557073678cd053209d17e2b05c335d4ee
Philipp Hoch <mail@philipp-hoch.de>
2022-11-18 13:46:52 +0100

    add launches schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const launchesSchmea = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+    default: 100,
+    min: 100,
+    max: 999,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  customers: [String],
+});
+
+module.exports = launchesSchmea;

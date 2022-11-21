62e28eea84ba1632f910c1f51749f009d989a263
Philipp Hoch <mail@philipp-hoch.de>
2022-11-21 17:40:01 +0100

    wip: launches mongo db

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -26,6 +26,19 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function saveLaunch(data) {
+  const launch = {
+    flightNumber: 100,
+    mission: "Kepler Exploration X",
+    rocket: "Explorer IS1",
+    launchDate: new Date("December 27, 2030"),
+    target: "Kepler-442 b",
+    customers: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true,
+  };
+}
+
 function addNewLaunch(launch) {
   latestFlightNumber++;
   launches.set(

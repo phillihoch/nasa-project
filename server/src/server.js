f8da5252206a51ceb7520da9deeabbf712797859
Philipp Hoch <mail@philipp-hoch.de>
2022-11-22 15:02:35 +0100

    update test environment

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,16 @@
 const http = require("http");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB conntion ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
